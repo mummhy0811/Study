2d184ccd247b4c1ca990ddc4c15250a68cf0bd94
mummhy0811 <mummhy0811@inu.ac.kr>
2023-01-10 17:27:39 +0900

    feat: 섹션4-5_스프링빈 추가 및 웹mvc

--- spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,8 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +16,7 @@ public class MemberService {
 
     //외부에서 멤버리포지토리를 넣어줌(DI)
     public MemberService(MemberRepository memberRepository){
+
         this.memberRepository=memberRepository;
     }
 
