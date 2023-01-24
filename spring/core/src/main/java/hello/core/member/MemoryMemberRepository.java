f65ea4364d0cbaf951d8917146119e5bde442948
mummhy0811 <mummhy0811@inu.ac.kr>
2023-01-24 16:49:55 +0900

    feat: 회원도메인 추가

---
+++ spring/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+    //db가 확정이 되지 않았다는 설정 -> 일시적인 메모리 리포지토리 사용 (테스트용)
+    private static Map<Long, Member> store=new HashMap<>();
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
