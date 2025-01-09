019ede39c1ad7a315700174ff07a8cb65c0bf2fd
Hyeyoung Moon <mummhy0811@gmail.com>
2025-01-09 18:54:47 +0900

    Chore: init jpa2

---
+++ spring/jpa/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+}
